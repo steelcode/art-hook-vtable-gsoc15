30e1a52573e0c65821e417bf2ec67affc6bb8039
valerio costamagna <valerio.costamagna@di.unito.it>
2015-08-09 18:09:05 +0200

    changed Android.mk with new adbi path

--- hooking/jni/Android.mk
+++ hooking/jni/Android.mk
@@ -19,13 +19,13 @@ LOCAL_MODULE    := libarthook
 LOCAL_SRC_FILES := main.c  artstuff.c  epoll_arm.c.arm 
 LOCAL_SRC_FILES += utils.c arthook_helper.c arthook_manager.c
 LOCAL_SRC_FILES += jni_wrappers.c  arthook_bridge.c hook_demo.c
-LOCAL_C_INCLUDES := ../../../adbi/instruments/base/
-#LOCAL_INCLUDE   := ../../../adbi/instruments/base/hook.h
+LOCAL_C_INCLUDES := ../../adbi/instruments/base/
+#LOCAL_INCLUDE   := ../../adbi/instruments/base/hook.h
 LOCAL_CFLAGS := -g -std=gnu99
 LOCAL_ARM_MODE := thumb
 LOCAL_SHARED_LIBRARIES := dl
 LOCAL_LDLIBS := -llog 
-LOCAL_LDLIBS += -Wl,--start-group ../../../adbi/instruments/base/obj/local/armeabi/libbase.a -Wl,--end-group
+LOCAL_LDLIBS += -Wl,--start-group ../../adbi/instruments/base/obj/local/armeabi/libbase.a -Wl,--end-group
 #LOCAL_STATIC_LIBRARIES := ../../../adbi/instruments/base/obj/local/armeabi/libbase.a 
 include $(BUILD_SHARED_LIBRARY)
 
