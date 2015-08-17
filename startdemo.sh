4b82647890465439f3437740008e1f0b1a94cbe9
valerio costamagna <valerio.costamagna@di.unito.it>
2015-08-17 15:16:43 +0200

    removed -D from am

--- startdemo.sh
+++ startdemo.sh
@@ -1,13 +1,36 @@
 #!/bin/bash
 
+echo "checking if checkjni is enabled..."
+
+RES=`adb shell 'getprop' | grep 'checkjni' | cut -d : -f 2`
+
+if [ -z $RES ]; then
+    echo "checkjni is mandatory on real-world devices..."
+    echo "it will be auto-enabled for you..."
+    adb shell stop
+    adb shell setprop dalvik.vm.checkjni true
+    adb shell start
+    echo "checkjni activated!! "
+fi
+
+if [ -n $RES ]; then
+    echo "ok, checkjni is already enabled!"
+    echo "moving on.."
+fi
+
+adb wait-for-device
+
+sleep 1
 
 adb shell 'cd /data/local/tmp; sh init.sh;'
 
-adb install -r app.apk
+adb install -r examples/app-testing.apk
 
 echo -e "starting testing app"
 
-adb shell "am start -D -W -n org.sid.testingmalwareapp/.MainActivity"
+
+# -D for wait debugger, problem with classloader..
+adb shell "am start -W -n org.sid.testingmalwareapp/.MainActivity"
 
 
 MYPID=`adb shell ps | grep testing | awk '{print $2}'`
@@ -21,7 +44,7 @@ adb shell chown $MYUID:$MYUID /data/local/tmp/dex/opt
 
 adb shell "cd /data/local/tmp; "
 
-sleep 5
+sleep 2 
 
 echo -e "starting hooking...."
 
